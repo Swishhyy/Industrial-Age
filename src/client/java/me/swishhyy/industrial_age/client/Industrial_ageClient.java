3080e305ce3976c7273e3fcf02d0001da9476607
Swishhyy <dylan.minatrea@gmail.com>
2025-03-05 23:19:08 -0600

    Project Init

---
+++ src/client/java/me/swishhyy/industrial_age/client/Industrial_ageClient.java
@@ -0,0 +1,10 @@
+package me.swishhyy.industrial_age.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Industrial_ageClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/swishhyy/industrial_age/Industrial_age.java
@@ -0,0 +1,10 @@
+package me.swishhyy.industrial_age;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Industrial_age implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
