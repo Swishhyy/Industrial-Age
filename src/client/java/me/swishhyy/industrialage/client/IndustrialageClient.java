a6a6cb4a41dcd101d858eb8ffbe3f19ceb7d1b6a
Swishhyy <72903966+Swishhyy@users.noreply.github.com>
2025-03-07 15:06:04 -0600

    Set to Fabric 1.21.1 instead of 1.21.4
    This is due to changes i don't like so I'm going back to a version I'm more familiar with. However i may update it in the future

---
+++ src/client/java/me/swishhyy/industrialage/client/IndustrialageClient.java
@@ -0,0 +1,10 @@
+package me.swishhyy.industrialage.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IndustrialageClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/swishhyy/industrialage/Industrialage.java
@@ -0,0 +1,10 @@
+package me.swishhyy.industrialage;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Industrialage implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
